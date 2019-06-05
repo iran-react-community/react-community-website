e1e6a502f71711eb3a8ac6249b0046dd2f27e5ce
amerllica <amerllica@gmail.com>
2019-06-05 22:55:12 +0430

    Changing template based on bundle splitting.

--- src/utils/template.js
+++ src/utils/template.js
@@ -9,6 +9,7 @@ export default ({ markup, helmet }) => {
 				<body ${helmet.bodyAttributes.toString()}>
 					<div id="root">${markup}</div>
 					<script src="/dist/client.js" defer></script>
+					<script src="/dist/vendor.js" defer></script>
 				</body>
 			</html>`;
 };
