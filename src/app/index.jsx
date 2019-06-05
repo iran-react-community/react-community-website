ebe2c686a0533fb5188921adb5b6b7ca21a5241c
amerllica <amerllica@gmail.com>
2019-06-05 19:44:01 +0430

    Fix the Index based on splitting the main parts.

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,90 +1,23 @@
-import React, {Component} from 'react';
-import Helmet from "react-helmet";
-import {Switch, Route} from 'react-router-dom';
-import {NavLink} from 'react-router-dom';
-import styles from '~/styles/styles.pcss';
+import React from 'react';
+import Helmet from 'react-helmet';
+import Menu from '~/app/components/Menu';
+import Routes from '~/app/routes/Routes';
 
-class Menu extends Component {
-    render() {
-        return (
-            <div>
-                <ul>
-                    <li>
-                        <NavLink exact to={'/'} activeClassName={styles.active}>Homepage</NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName={styles.active} to={'/about'}>About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName={styles.active} to={'/contact'}>Contact</NavLink>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
-}
+const Index = () => (
+    <>
+        <Helmet
+            htmlAttributes={{lang: "en", amp: undefined}}
+            titleTemplate="%s | React App"
+            titleAttributes={{itemprop: "name", lang: "en"}}
+            meta={[
+                {name: "description", content: "Server side rendering example"},
+                {name: "viewport", content: "width=device-width, initial-scale=1"},
+            ]}
+            link={[{rel: "stylesheet", href: "/dist/styles.css"}]}
+        />
+        <Menu/>
+        <Routes/>
+    </>
+);
 
-
-class Homepage extends Component {
-
-    render() {
-        return (
-            <div className={styles.component}>
-                <Helmet title="Welcome to our Homepage"/>
-                <Menu/>
-                <h1>Homepage</h1>
-            </div>
-        );
-    }
-}
-
-class About extends Component {
-    render() {
-        return (
-            <div>
-                <Helmet title="About us"/>
-                <Menu/>
-                <h1>About</h1>
-            </div>
-        );
-    }
-}
-
-class Contact extends Component {
-    render() {
-        return (
-            <div>
-                <Helmet title="Contact us"/>
-                <Menu/>
-                <h1>Contact</h1>
-            </div>
-        );
-    }
-}
-
-
-export default class Index extends Component {
-
-    render() {
-        return (
-            <div>
-                <Helmet
-                    htmlAttributes={{lang: "en", amp: undefined}} // amp takes no value
-                    titleTemplate="%s | React App"
-                    titleAttributes={{itemprop: "name", lang: "en"}}
-                    meta={[
-                        {name: "description", content: "Server side rendering example"},
-                        {name: "viewport", content: "width=device-width, initial-scale=1"},
-                    ]}
-                    link={[{rel: "stylesheet", href: "/dist/styles.css"}]}
-                    script={[{src: "/dist/client.js", type: "text/javascript", defer: true}]}
-                />
-                <Switch>
-                    <Route exact path='/' component={Homepage}/>
-                    <Route path='/about' component={About}/>
-                    <Route path='/contact' component={Contact}/>
-                </Switch>
-            </div>
-        );
-    }
-}
+export default Index;
