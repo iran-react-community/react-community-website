eeae7cc6c66d67063672e7634a83fce2108d4c62
amerllica <amerllica@gmail.com>
2019-06-08 22:46:44 +0430

    Add related classNames to the every tag of menu.

--- src/app/components/Menu.jsx
+++ src/app/components/Menu.jsx
@@ -1,6 +1,6 @@
-import React, {memo} from 'react';
-import {NavLink} from "react-router-dom";
-import styles from "~/styles/styles.pcss";
+import React, { memo } from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from '~/styles/styles.pcss';
 import routesNames from '~/utils/routesNames';
 import contentWords from '~/utils/contentWords';
 import logo from '~/assets/iran-react-community.png';
@@ -19,17 +19,17 @@ const {
 
 const Menu = () => (
     <ul className={styles.menu}>
-        <li>
-            <NavLink exact to={toHome} activeClassName={styles.active}>{home}</NavLink>
+        <li className={styles['menu__item']}>
+            <NavLink exact to={toHome} activeClassName={styles['menu__active']}>{home}</NavLink>
         </li>
-        <li>
-            <NavLink to={toAboutUs} activeClassName={styles.active}>{aboutUs}</NavLink>
+        <li className={styles['menu__item']}>
+            <NavLink to={toAboutUs} activeClassName={styles['menu__active']}>{aboutUs}</NavLink>
         </li>
-        <li>
-            <NavLink to={toContactUs} activeClassName={styles.active}>{contactUs}</NavLink>
+        <li className={styles['menu__item']}>
+            <NavLink to={toContactUs} activeClassName={styles['menu__active']}>{contactUs}</NavLink>
         </li>
-        <li>
-            <NavLink to={toContactUs} activeClassName={styles.active}>
+        <li className={styles['menu__item']}>
+            <NavLink to={toHome} activeClassName={styles['menu__active']}>
                 <img src={logo} alt="iran-react-community-logo"/>
             </NavLink>
         </li>
