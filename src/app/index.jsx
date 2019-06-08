429e9c19fb43a8331c4a4ad2eadad841aa7fcbd7
amerllica <amerllica@gmail.com>
2019-06-08 22:20:44 +0430

    Change the information of application index file.

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -7,10 +7,10 @@ const Index = () => (
     <>
         <Helmet
             htmlAttributes={{lang: "en", amp: undefined}}
-            titleTemplate="%s | React App"
+            titleTemplate="%s | iran-react-community"
             titleAttributes={{itemprop: "name", lang: "en"}}
             meta={[
-                {name: "description", content: "Server side rendering example"},
+                {name: "description", content: "Iran React Community"},
                 {name: "viewport", content: "width=device-width, initial-scale=1"},
             ]}
             link={[{rel: "stylesheet", href: "/dist/styles.css"}]}
