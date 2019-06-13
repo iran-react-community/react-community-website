ec6f824861702f0f0f88f38e11f4a948604945da
amerllica <amerllica@gmail.com>
2019-06-13 22:27:28 +0430

    Completing the Home page of iran react community.

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, {memo} from 'react';
 import Helmet from 'react-helmet';
+import Socials from '~/app/components/Socials';
 import contentWords from '~/utils/contentWords';
 import styles from "~/styles/styles.pcss";
 
@@ -7,12 +8,24 @@ const {
     menu: {
         home,
     },
+    page: {
+        home: {
+            title: {
+                fa,
+                en,
+            },
+            subTitle,
+        },
+    },
 } = contentWords;
 
 const Home = () => (
-    <div className={styles.component}>
+    <div className={styles.home}>
         <Helmet title={home}/>
-        <h1>{home}</h1>
+        <h1 className={styles['home__en-title']}>{en}</h1>
+        <h2 className={styles['home__fa-title']}>{fa}</h2>
+        <h3 className={styles['home__sub-title']}>{subTitle}</h3>
+        <Socials/>
     </div>
 );
 
