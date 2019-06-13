f507e33cb3d92ff9e2b2aab0376fc416dc7bbb32
amerllica <amerllica@gmail.com>
2019-06-14 02:58:19 +0430

    Make contact us page.

--- src/app/pages/ContactUs.jsx
+++ src/app/pages/ContactUs.jsx
@@ -1,18 +1,37 @@
 import React, {memo} from 'react';
-import Helmet from 'react-helmet';
+import KopiLeft from '~/app/components/KopiLeft';
+import Socials from '~/app/components/Socials';
+import Paper from '~/app/components/Paper';
 import contentWords from '~/utils/contentWords';
 import styles from "~/styles/styles.pcss";
 
 const {
-    menu: {
-        contactUs
+    page: {
+        contactUs: {
+            address,
+            mail,
+            city,
+            email,
+        },
     },
 } = contentWords;
 
 const ContactUs = () => (
-    <div className={styles.component}>
-        <Helmet title={contactUs}/>
-        <h1>{contactUs}</h1>
+    <div className={styles['contact-us']}>
+        <KopiLeft/>
+        <Socials/>
+        <div className={styles['contact-us__paper-wrapper']}>
+            <Paper>
+                <i className={styles['icon-location']}/>
+                <span>{address}</span>
+                <span>{city}</span>
+            </Paper>
+            <Paper>
+                <i className={styles['icon-mail']}/>
+                <span>{mail}</span>
+                <a href={`mailto:${email}`}>{email}</a>
+            </Paper>
+        </div>
     </div>
 );
 
